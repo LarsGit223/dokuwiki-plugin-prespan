8811bb4da35258575237fe1f6c4ff052acd29857
LarsGit223 <lars_paulsen@web.de>
2018-09-23 15:32:33 +0200

    Fixed signature errors.

--- syntax.php
+++ syntax.php
@@ -61,7 +61,7 @@ class syntax_plugin_prespan extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, Doku_Handler $handler) {
         switch ($state) {
           case DOKU_LEXER_ENTER : 
             break;
@@ -80,7 +80,7 @@ class syntax_plugin_prespan extends DokuWiki_Syntax_Plugin {
     /**
      * Create output
      */
-    function render($mode, &$renderer, $data) {
+    function render($mode, Doku_Renderer $renderer, $data) {
         if($mode == 'xhtml'){
             if ($data[1] == DOKU_LEXER_ENTER){
                 $renderer->doc .= '<span class="prespan">';
