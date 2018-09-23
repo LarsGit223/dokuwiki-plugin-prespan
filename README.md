d9cc06d52f50a72aa487de23c453f72281c1b707
LarsGit223 <lars_paulsen@web.de>
2018-09-23 09:54:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# DokuWiki Prespan plugin
+
+A plugin for [DokuWiki](https://www.dokuwiki.org/) which allows users create text
+spans which preserve spaces.
+
+## Usage
+
+When you have it installed, use the following syntax ```<prespan>text</prespan>```.
+Between the ```<prespan>``` tags other formatting is allowed. That means you
+e.g. could create a underlined text span with preserved space by writing
+```<prespan>__text__</prespan>```.
+
+## Limitations
+
+None.
+
+## Download / Installation
+
+ See [DokuWiki's plugin page](https://www.dokuwiki.org/plugin:prespan)
+
+Alternatively
+ 1. Download plugin/repo archive
+ 2. Installing the plugin:
+    * You can either extract the archive into the ```lib/plugins``` directory or
+    * Load the admin page, go to the Extension Manager and enter the URL of the plugin under Manual Install.
+
+## Authors
+
+ The plugin has been written by LarsDW223.
+
+## License
+
+This plugin is open-source and licensed under the GNU GPL v.2.
---
+++ syntax.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * wpre Plugin: for output using word wrapped preformatted text
+ * Syntax:     <wpre> text </wpre>
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Max Binshtok (max.binshtok@gmail.com)
+ */
+ 
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+ 
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_prespan extends DokuWiki_Syntax_Plugin {
+ 
+    /**
+     * return some info
+     */
+    function getInfo(){
+        return array(
+          'author' => 'Max Binshtok',
+          'email'  => 'max.binshtok@gmail.com',
+          'date'   => '2010-02-12',
+          'name'   => 'wpre Plugin',
+          'desc'   => 'for output using word wrapped preformatted text',
+          'url'    => 'http://www.dokuwiki.org/plugin:wpre',
+        );
+    }
+ 
+    /**
+     * What kind of syntax are we?
+     */
+    function getType(){
+        return 'formatting';
+    }
+
+ 
+    /**
+     * What about paragraphs? (optional)
+     */
+   function getPType(){
+       return 'normal';
+   }
+ 
+    /**
+     * Where to sort in?
+     */
+    function getSort(){
+        return 195;
+    }
+
+    function getAllowedTypes(){
+        return array('formatting', 'substition', 'disabled');
+    }
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+        $this->Lexer->addEntryPattern('<prespan>(?=.*</prespan>)', $mode, 'plugin_prespan');
+    }
+
+    function postConnect() {
+        $this->Lexer->addExitPattern('</prespan>', 'plugin_prespan');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){
+        switch ($state) {
+          case DOKU_LEXER_ENTER : 
+            break;
+          case DOKU_LEXER_MATCHED :
+            break;
+          case DOKU_LEXER_UNMATCHED :
+            break;
+          case DOKU_LEXER_EXIT :
+            break;
+          case DOKU_LEXER_SPECIAL :
+            break;
+        }
+        return array($match, $state);
+    }
+ 
+    /**
+     * Create output
+     */
+    function render($mode, &$renderer, $data) {
+        if($mode == 'xhtml'){
+            if ($data[1] == DOKU_LEXER_ENTER){
+                $renderer->doc .= '<span class="prespan">';
+            } else if ($data[1] == DOKU_LEXER_UNMATCHED){
+                $renderer->doc .= $renderer->_xmlEntities($data[0]);
+            } else if ($data[1] == DOKU_LEXER_EXIT){
+                $renderer->doc .= '</span>';
+            }
+            return true;
+        }
+        return false;
+    }   
+}
+ 
+//Setup VIM: ex: et ts=4 enc=utf-8 :
+ 
+?>
